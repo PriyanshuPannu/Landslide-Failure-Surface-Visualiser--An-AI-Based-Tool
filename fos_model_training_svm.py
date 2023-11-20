2d7a062a70d14b52492008d10be38b7f7b8f7315
PriyanshuPannu <112780053+PriyanshuPannu@users.noreply.github.com>
2023-11-20 12:04:50 +0530

    Update fos_model_training_svm.py

--- fos_model_training_svm.py
+++ fos_model_training_svm.py
@@ -4,11 +4,14 @@ import pandas as pd
 from sklearn.svm import SVR
 
 data=pd.read_excel("data.xlsx")
+data=data.dropna()
 
-input_feature_x=data.iloc[:,0:2].values
-output_y=data.iloc[:,2].values
+input_feature_x=data.iloc[:,0:6].values
+output_y=data.iloc[:,6].values
 model = SVR(kernel = 'rbf')
+print("Training started.....")
 model.fit(input_feature_x, output_y)
 
 filename = 'fos_model.sav'
 pickle.dump(model, open(filename, 'wb'))
+print("Training completed!!")
